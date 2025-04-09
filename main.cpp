0a67a3500c7a6fde13e3880304e36991840ea7f4
LouieUbert <Lubert@mail.bradley.edu>
2025-04-09 14:58:45 -0500

    Combined milestones 1-4

--- main.cpp
+++ main.cpp
@@ -4,11 +4,10 @@
 #include <vector>
 using namespace std;
 
-// Class to handle reading CSV files (Task 3)
 class CSVReader {
 public:
     static vector<vector<string>> readCSV(const string& filename) {
-        ifstream file("SchoolLists/"+filename);
+        ifstream file("cmake-build-debug/SchoolLists/"+filename);
         vector<vector<string>> data;
         string line, word;
 
@@ -26,81 +25,283 @@ public:
             data.push_back(row);
         }
         file.close();
+
+        if (data.empty()) {
+            cerr << "Error: No data loaded from file." << endl;
+        }
+
         return data;
     }
 };
 
-// Structure to store school data (Task 1)
 struct School {
     string name, address, city, state, county;
     School* next;
 
-    // Constructor to initialize school object
     School(string n, string a, string c, string s, string co)
         : name(n), address(a), city(c), state(s), county(co), next(nullptr) {}
 };
+
+struct TreeNode {
+    School* school;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(School* school) : school(school), left(nullptr), right(nullptr) {}
+};
+
+class SchoolList {
+private:
+    School* head;
+
+public:
+    SchoolList() : head(nullptr) {}
+
+    void insertFirst(School* school) {
+        school->next = head;
+        head = school;
+    }
+
+    void insertLast(School* school) {
+        if (!head) {
+            head = school;
+            return;
+        }
+        School* temp = head;
+        while (temp->next)
+            temp = temp->next;
+        temp->next = school;
+    }
+
+    void deleteByName(const string& name) {
+        School* temp = head, *prev = nullptr;
+        while (temp && temp->name != name) {
+            prev = temp;
+            temp = temp->next;
+        }
+        if (!temp) {
+            cout << "School not found.\n";
+            return;
+        }
+        if (!prev)
+            head = temp->next;
+        else
+            prev->next = temp->next;
+        delete temp;
+    }
+
+    School* findByName(const string& name) {
+        School* temp = head;
+        while (temp) {
+            if (temp->name == name)
+                return temp;
+            temp = temp->next;
+        }
+        return nullptr;
+    }
+
+    void display() {
+        School* temp = head;
+        while (temp) {
+            cout << temp->name << ", " << temp->address << ", "
+                 << temp->city << ", " << temp->state << ", "
+                 << temp->county << endl;
+            temp = temp->next;
+        }
+    }
+};
+
+class SchoolBST {
+     private:
+     TreeNode* root;
+
+     TreeNode* insert(TreeNode* node, School* school) {
+         if (node == nullptr) {
+             return new TreeNode(school);
+         } if (school->name < node->school->name) {
+             node->left = insert(node->left, school);
+         } else {
+             node->right = insert(node->right, school);
+         } return node;
+     }
+
+     TreeNode* findMin(TreeNode* node) {
+         while (node && node->left)
+             node = node->left;
+         return node;
+     }
+
+     TreeNode* deleteByName(TreeNode* node, const string& name) {
+         if (node == nullptr) return nullptr;
+         if (name < node->school->name) {
+             node->left = deleteByName(node->left, name);
+         } else if (name > node->school->name) {
+             node->right = deleteByName(node->right, name);
+         } else {
+             //Node with less than 2 children
+             if (!node->left) {
+                 TreeNode* temp = node->right;
+                 delete node;
+                 return temp;
+             } else if (!node->right) {
+                 TreeNode* temp = node->left;
+                 delete node;
+                 return temp;
+             }
+
+             //Node with 2 children
+             TreeNode* successor = findMin(node->right);
+             node->school = successor->school;
+             node->right = deleteByName(node->right, successor->school->name);
+         }
+     }
+
+     TreeNode* findByName(TreeNode* node, const string& name) {
+         if (!node || node->school->name == name) {
+             return node;
+         } if (node->school->name > name) {
+             return findByName(node->left, name);
+         } return findByName(node->right, name);
+     }
+
+     void displayInOrder(TreeNode* node) {
+         if (!node) return;
+         displayInOrder(node->left);
+         cout << node->school->name << "," << node->school->address << endl;
+         displayInOrder(node->right);
+     }
+
+     void displayPreOrder(TreeNode* node) {
+         if (!node) return;
+         cout << node->school->name << "," << node->school->address << endl;
+         displayPreOrder(node->left);
+         displayPreOrder(node->right);
+     }
+
+     void displayPostOrder(TreeNode* node) {
+         if (!node) return;
+         displayPostOrder(node->left);
+         displayPostOrder(node->right);
+         cout << node->school->name << "," << node->school->address << endl;
+     }
+
+     public:
+     SchoolBST() : root(nullptr) {}
+
+     void insert(School* school) {
+         root = insert(root, school);
+     }
+
+     TreeNode* findMin() {
+         return findMin(root);
+     }
+
+     TreeNode* deleteByName(const string& name) {
+         root = deleteByName(root, name);
+     }
+
+     School* findByName(const string& name) {
+         TreeNode* node = findByName(root, name);
+         return node ? node->school : nullptr;
+     }
+
+     void displayInOrder() {
+         displayInOrder(root);
+     }
+
+     void displayPreOrder() {
+         displayPreOrder(root);
+     }
+
+     void displayPostOrder() {
+         displayPostOrder(root);
+     }
+ };
 
 class SchoolHashTable {
 private:
     static const int TABLE_SIZE = 100;
     vector<School*> table;
 
-    int polynomialHash(string key, int tableSize = TABLE_SIZE, int prime = 31) {
-        long hash = 0;
-        long power = 1;
+    int hashFunction(string key, int tableSize = TABLE_SIZE) {
+        int hash = 0;
         for (char ch : key) {
-            hash = (hash + (ch - 'a' + 1) * power) % tableSize;
-            power = (power * prime) % tableSize;
+            hash += ch;
         }
-        return hash;
+        return hash % tableSize;
     }
 
 public:
     SchoolHashTable() : table(TABLE_SIZE, nullptr) {}
 
     void insert(School* School) {
-        int index = polynomialHash(School->name);
+        int index = hashFunction(School->name);
         School->next = table[index];
         table[index] = School;
     }
+
+    void deleteByName(const string& name) {
+        int index = hashFunction(name);
+        School* temp = table[index];
+        School* prev = nullptr;
+
+        while (temp && temp->name != name) {
+            prev = temp;
+            temp = temp->next;
+        }
+
+        if (!temp) {
+            cout << "School not found.\n";
+            return;
+        }
+
+        if (!prev)
+            table[index] = temp->next;
+        else
+            prev->next = temp->next;
+
+        delete temp;
+    }
+
+
+    School* findByName(const string& name) {
+        int index = hashFunction(name);
+        School* School = table[index];
+        while (School) {
+            if (School->name == name) {
+                return School;
+            }
+            School = School->next;
+        }
+        return nullptr;
+    }
+
+    void display() {
+        for (int i = 0; i < TABLE_SIZE; i++) {
+            if (table[i]) {
+                School* temp = table[i];
+                cout << "Index " << i << ": ";
+                while (temp) {
+                    cout << temp->name << ", " << temp->address << "" << temp->city << temp->state << temp->county << endl;
+                    cout << temp->name << ", " << temp->address << " " << temp->city << ", " << temp->state << ", " << temp->county << endl;
+                    temp = temp->next;
+                }
+            }
+        }
+    }
 };
 
-// Main function to load data, manage the linked list, and allow user interaction (Task 4)
 int main() {
+    SchoolList list;
     SchoolHashTable hashTable;
     vector<vector<string>> data = CSVReader::readCSV("Illinois_Schools.csv");
 
-    // Load data into linked list, skipping the header row
     for (size_t i = 1; i < data.size(); ++i) {
-        if (data[i].size() == 5)
+        if (data[i].size() == 5) {
+            list.insertLast(new School(data[i][0], data[i][1], data[i][2], data[i][3], data[i][4]));
             hashTable.insert(new School(data[i][0], data[i][1], data[i][2], data[i][3], data[i][4]));
+        }
     }
 
-    int choice;
-    string name;
-    do {
-        cout << "1. Display Schools\n2. Find School\n3. Delete School\n4. Exit\nChoice: ";
-        cin >> choice;
-        cin.ignore();
-
-        switch (choice) {
-            case 1:
-                hashTable.display();
-                break;
-            case 2:
-                cout << "Enter school name: ";
-                getline(cin, name);
-                if (School* school = hashTable.findByName(name))
-                    cout << school->name << ", " << school->address << "\n";
-                else
-                    cout << "School not found.\n";
-                break;
-            case 3:
-                cout << "Enter school name: ";
-                getline(cin, name);
-                hashTable.deleteByName(name);
-                break;
-        }
-    } while (choice != 4);
     return 0;
+
 }
